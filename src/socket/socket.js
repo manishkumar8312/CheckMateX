c8f3c26bb751e2a1540ab1cce8c7f2fa6d7eedbd
Manish Kumar Sah <codeisfunforme@gmail.com>
2026-01-28 21:35:09 +0530

    changed

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3001';
+console.log('Socket URL:', SOCKET_URL, 'Env var:', import.meta.env.VITE_SOCKET_URL);
 
 class SocketService {
   constructor() {
